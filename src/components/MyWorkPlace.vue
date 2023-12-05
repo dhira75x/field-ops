c9c4a23e5f27f62b60bdf0012264032ab8592309
phareedah <fariffarida@gmail.com>
2023-12-05 12:56:33 +0100

    my commit

--- src/components/MyWorkPlace.vue
+++ src/components/MyWorkPlace.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-[1200px] bg-white">
+  <div class="w-full h-[1000px] bg-white">
     <div class="mb-4 bg-gray-200">
       <nav class="mt-1 p-2 border text-black-300 w-full flex flex-row rounded-md">
         <div class="grid grid-cols-1">
@@ -120,6 +120,7 @@
           </button>
         </div>
       </div>
+
       <div class="w-full my-20">
         <form class="w-full h-[1200px] mx-20 justify-items-center">
           <div class="shadow-lg w-[970px] h-[120px] mx-24">
@@ -173,88 +174,8 @@
                   />
                 </svg>
               </button>
-              <div
-                v-if="isVisible"
-                id="dropdownDefaultCheckbox"
-                class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-              >
-                <ul
-                  class="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="dropdownCheckboxButton"
-                >
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-1"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-1"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        checked
-                        id="checkbox-item-2"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-2"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-3"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-3"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-3"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-3"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <div class="flex items-center">
-                    <label for="comment" class="block text-sm font-medium text-gray-600"
-                      >Comments</label
-                    >
-                    <textarea
-                      id="comment"
-                      name="comment"
-                      rows="3"
-                      class="mt-1 p-2 border w-full rounded-md"
-                    ></textarea>
-                  </div>
-                </ul>
+              <div>
+                <CheckList />
               </div>
             </div>
           </div>
@@ -307,88 +228,8 @@
                   />
                 </svg>
               </button>
-              <div
-                v-if="isVisible"
-                id="dropdownDefaultCheckbox"
-                class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-              >
-                <ul
-                  class="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="dropdownCheckboxButton"
-                >
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-1"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-1"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        checked
-                        id="checkbox-item-2"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-2"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-3"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-3"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-3"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-3"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <div class="flex items-center">
-                    <label for="comment" class="block text-sm font-medium text-gray-600"
-                      >Comments</label
-                    >
-                    <textarea
-                      id="comment"
-                      name="comment"
-                      rows="3"
-                      class="mt-1 p-2 border w-full rounded-md"
-                    ></textarea>
-                  </div>
-                </ul>
+              <div>
+                <CheckList />
               </div>
             </div>
           </div>
@@ -443,88 +284,8 @@
                   />
                 </svg>
               </button>
-              <div
-                v-show="isVisible"
-                id="dropdownDefaultCheckbox"
-                class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-              >
-                <ul
-                  class="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="dropdownCheckboxButton"
-                >
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-1"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-1"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        checked
-                        id="checkbox-item-2"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-2"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-3"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-3"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-3"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-3"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <div class="flex items-center">
-                    <label for="comment" class="block text-sm font-medium text-gray-600"
-                      >Comments</label
-                    >
-                    <textarea
-                      id="comment"
-                      name="comment"
-                      rows="3"
-                      class="mt-1 p-2 border w-full rounded-md"
-                    ></textarea>
-                  </div>
-                </ul>
+              <div>
+                <CheckList />
               </div>
             </div>
           </div>
@@ -577,88 +338,8 @@
                   />
                 </svg>
               </button>
-              <div
-                v-if="isVisible"
-                id="dropdownDefaultCheckbox"
-                class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-              >
-                <ul
-                  class="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="dropdownCheckboxButton"
-                >
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-1"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-1"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        checked
-                        id="checkbox-item-2"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-2"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-3"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-3"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        id="checkbox-item-3"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        for="checkbox-item-3"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >Ask for permission to record the session</label
-                      >
-                    </div>
-                  </li>
-                  <div class="flex items-center">
-                    <label for="comment" class="block text-sm font-medium text-gray-600"
-                      >Comments</label
-                    >
-                    <textarea
-                      id="comment"
-                      name="comment"
-                      rows="3"
-                      class="mt-1 p-2 border w-full rounded-md"
-                    ></textarea>
-                  </div>
-                </ul>
+              <div>
+                <CheckList />
               </div>
             </div>
           </div>
@@ -669,6 +350,7 @@
 </template>
 <script setup>
 import { ref } from 'vue'
+import CheckList from './CheckList.vue'
 
 const isVisible = ref(true)
 
