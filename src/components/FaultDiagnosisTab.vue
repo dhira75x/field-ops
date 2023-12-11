cf2a20bef2f7b4ad557013573215f0044b459749
phareedah <fariffarida@gmail.com>
2023-12-11 12:49:01 +0100

    fault diagnosis report

---
+++ src/components/FaultDiagnosisTab.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="grid grid-cols-2 w-[1278px] h-[51px] bg-black">
+    <div class="justify-start mx-5">
+      <div class="text-white text-lg font-normal font-['Source Sans Pro']">
+        Fault Diagnosis Report
+      </div>
+    </div>
+    <div class="flex flex-row-reverse mr-10 gap-[43px]">
+      <div class="text-white text-lg font-normal font-['Source Sans Pro']">
+        July 29, 2023 11:48:08
+      </div>
+    </div>
+  </div>
+</template>
